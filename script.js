055b1d98f8aa703e2ea6961e8b5a4e55de015453
Andrew <ajdton@gmail.com>
2022-08-30 18:10:43 -0400

    Major design changes

--- script.js
+++ script.js
@@ -2,6 +2,31 @@
 
 const bestSources = document.getElementById("contain");
 const dropDown = document.querySelector("#source");
+const leftContacts = document.querySelector("#groups")
+const footer = document.querySelector("#center")
+const FooterEnd = document.querySelector("#end")
+
+//This is to make the facebookcontact disappear once it hits footer
+// footer.addEventListener(("click",() => {
+// 	e.leftContacts.style.display = "none"
+// 	console.log('hello')
+// }))
+
+const footerEnd = ()=>{
+	leftContacts.style.display = "none"
+	console.log('gone')
+}
+const footerBegin = ()=>{
+	leftContacts.style.display = "block"
+	console.log('block')
+}
+
+
+
+// footer.addEventListener(("mouseleave",() => {
+// 	leftContacts.style.display = "block"
+// }))
+//END OF IT
 
 const displaySources = () => {
 	dropDown.style.display = "block";
@@ -29,3 +54,10 @@ dropDown.addEventListener("mouseenter", enterSources);
 bestSources.addEventListener("mouseenter", displaySources);
 
 bestSources.addEventListener("mouseleave", leave);
+
+//DELETES FOOTER ONCE YOU GET TO THE BOTTOM
+footer.addEventListener("mouseenter",footerEnd);
+footer.addEventListener("mouseleave", footerBegin);
+FooterEnd.addEventListener("mouseenter", footerEnd);
+FooterEnd.addEventListener("mouseleave", footerBegin);
+
